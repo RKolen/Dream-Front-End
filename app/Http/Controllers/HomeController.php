c25dea764c95c8f1b338bc09eafc3e5f11617620
Riccardo Parrello <riccardoparrello@live.nl>
2018-03-16 13:53:30 +0100

    HomeController index now loads data from backend and passes it to view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,13 @@ class HomeController extends Controller
 {
 
     public function index(){
-        return view('home.index');
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, env('BACKEND_URL') . '/games');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        $games = json_decode($output);
+        return view('home.index', compact('games'));
     }
 
 }
