f4afc88fef62d5fa806c2bcd14abe324ea99c032
Riccardo Parrello <riccardoparrello@live.nl>
2018-03-18 21:36:12 +0100

    Added links to gamepages on homepage and fixed back to home button

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -13,14 +13,18 @@
 
 <h2>@lang('home.spotlight')</h2>
 <div class="section-container">
-    <div class="game-image" style="background-image: url('{{'http://' . env('BACKEND_URL') . '/games/' . $games->spotlight[0]->id . '/image'}}')";>
+    <div class="game-image" style="background-image: url('{{'http://' . env('BACKEND_URL') . '/games/' . $games->spotlight[0]->id . '/image'}}')">
+      <button type="button" class="invisible-button" onclick="location.href='{{'games/' . $games->spotlight[0]->id}}'">
+      </button>
     </div>
 </div>
 
 <h2>@lang('home.discover')</h2>
 <div class="section-container">
     @foreach ($games->discover as $discoverGame)
-      <div class="game-image-small" style="background-image: url('{{'http://' . env('BACKEND_URL') . '/games/' . $discoverGame->id . '/image'}}')";>
+      <div class="game-image-small" style="background-image: url('{{'http://' . env('BACKEND_URL') . '/games/' . $discoverGame->id . '/image'}}')">
+        <button type="button" class="invisible-button" onclick="location.href='{{'games/' . $discoverGame->id}}'">
+        </button>
       </div>
     @endforeach
 </div>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav id="navbar">
 
   <ul>
-    <li><a class="active" href="#">@lang('general.home')</a></li>
+    <li><a href="/">@lang('general.home')</a></li>
     <li><a href="#">@lang('general.store')</a></li>
   </ul>
 
