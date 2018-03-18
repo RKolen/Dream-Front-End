785a459f26aef4cd36ebcffb8ba1bab133679617
Riccardo Parrello <riccardoparrello@live.nl>
2018-03-18 23:35:49 +0100

    Added particlesjs source file to project

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -21,7 +21,7 @@
     </div>
 
     <div id="particles-js"></div>
-    <script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+    <script src="{{ URL::asset('js/particles-source.js') }}"></script>
     <script src="{{ URL::asset('js/particles.js') }}"></script>
   </body>
 </html>
