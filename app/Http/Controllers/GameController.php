ba2ba36ed08eaf7970034e0db20fd9e747f4470d
Riccardo Parrello <riccardoparrello@live.nl>
2018-03-18 21:08:39 +0100

    Created individual pages for games

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
-    //
+  public function show($game)
+  {
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, env('BACKEND_URL') . '/games/' . $game);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      $output = curl_exec($ch);
+      curl_close($ch);
+      $gameinfo = json_decode($output);
+      return view('game.index', compact('gameinfo'));
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,5 @@
 */
 
 Route::get('/', 'HomeController@index');
+
+Route::get('/games/{game}', 'GameController@show');
