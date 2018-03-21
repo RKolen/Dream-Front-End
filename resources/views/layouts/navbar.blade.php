fe416cf67960f165eab0fe7ad79eaff63bb2eb7d
Riccardo Parrello <riccardoparrello@live.nl>
2018-03-21 13:27:42 +0100

    Adjust navbar based on login status

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,7 +4,12 @@
     <li><a href="/">@lang('general.home')</a></li>
     <li><a href="#">@lang('general.store')</a></li>
     <div class="spacer"></div>
-    <li><a href="/login">@lang('general.login')</a></li>
-  </ul>
 
+    @if ($logininfo->loggedin)
+    <li><a href="#">{{$logininfo->name}}</a></li>
+    <li><a href="/logout">@lang('general.logout')</a></li>    
+    @else
+    <li><a href="/login">@lang('general.login')</a></li>
+    @endif
+  </ul>
 </nav>
